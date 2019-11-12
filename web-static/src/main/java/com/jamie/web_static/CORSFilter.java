47a82b65356282e18e31af10f8843eb19bb2b5be
Jamie-Jin <18688231157@163.com>
2019-11-12 08:05:07 +0800

    添加静态资源模块

---
+++ web-static/src/main/java/com/jamie/web_static/CORSFilter.java
@@ -0,0 +1,24 @@
+package com.jamie.web_static;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORSFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
